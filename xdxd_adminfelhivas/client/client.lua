1697c59e72836bba8de3dd44cd393d0b5d90a383
xd.xd <154706368+xdxd8@users.noreply.github.com>
2024-05-02 16:31:49 +0200

    Delete xdxd_adminfelhivas directory

--- xdxd_adminfelhivas/client/client.lua
+++
@@ -1,8 +0,0 @@
-RegisterNetEvent('xdxd_adminfelhivas:showMessage', function(message)
-    SendNUIMessage({
-        type = "adminAnnounce",
-        message = message
-    })
-    PlaySoundFrontend(-1, "Mission_Pass_Notify", "DLC_HEISTS_GENERAL_FRONTEND_SOUNDS", false)
-
-end)
